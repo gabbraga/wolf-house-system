8676a25307a710f4f39d6d07d341d258a6c5919d
Gabriela Braga <bragaga@csc-scc.gc.ca>
2020-12-20 16:26:53 -0500

    add get and post endpoints

--- server/controllers/member-controller.js
+++ server/controllers/member-controller.js
@@ -1,21 +1,45 @@
 'use strict'
 
 const router = require("express").Router();
-const member = require("../models/member");
+const Member = require("../models/member");
 
 //get all wolf members
 router.get("/", function (req, res) {
-    res.end();
+    Member.find()
+        .exec()
+        .then(members=>{
+            res.status(200).json(members);
+        })
+        .catch(err=> {
+            console.log(err);
+        })
+        .finally(()=>{
+            res.end();
+        });
 });
 
 //get a wolf member, specified by ID
 router.get("/:id", function (req, res) {
-
+    
 });
 
 // add a new wolf member
 router.post("/", function (req, res) {
-    
+    const member = new Member({
+        name: req.body.name,
+        house: req.body.house,
+        type: req.body.type
+    });
+    member.save().then(result=> {
+        console.log(result); 
+    })
+    .catch(err=>{
+        console.log(err);
+    });
+    res.status(201).json({
+        message:"handling post",
+        createdMember: member
+    });
 });
 
 //edit a wolf member, specified by ID
