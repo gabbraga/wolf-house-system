17bb91d8a93160c7f48a90d08d97658d2f7d29b0
Gabriela Braga <bragaga@csc-scc.gc.ca>
2020-12-21 08:07:03 -0500

    added global setting for mongoose's findOneAndUpdate

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,9 @@
 //connect to the database 'wolfhouse'
 const mongoose = require('mongoose');
 const connection = mongoose.connect('mongodb://localhost:27017/wolfhouse_dev', {useNewUrlParser: true});
+// Make Mongoose use `findOneAndUpdate()`. Note that this option is `true`
+// by default, you need to set it to false.
+mongoose.set('useFindAndModify', false);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
